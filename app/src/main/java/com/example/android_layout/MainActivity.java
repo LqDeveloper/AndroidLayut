37f109f0ce0494d5e54bf93f9346b997428335c4
Quan Li <quan.li@williamoneilchina.com>
2019-06-05 11:23:50 +0800

    安卓布局

---
+++ app/src/main/java/com/example/android_layout/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android_layout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("主页");
+        goToActivity();
+    }
+
+
+    private void  goToActivity(){
+        TextView linearTextView = findViewById(R.id.linearLayout);
+        linearTextView.setOnClickListener((view)->{
+            Intent intent = new Intent(MainActivity.this,LinearActivity.class);
+            startActivity(intent);
+        });
+
+        TextView tableTextView = findViewById(R.id.tableLayout);
+        tableTextView.setOnClickListener((view)->{
+            Intent intent = new Intent(MainActivity.this,TableActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android_layout/TableActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+        setTitle("表格布局");
+    }
+}
